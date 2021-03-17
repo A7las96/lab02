3e0796535e35ced3b70466a079767d869cd6f165
A7las96 <philipptyrnov@gmail.com>
2021-03-17 09:25:25 +0300

    HelloWorld new style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,18 @@
-#include <string>
 #include <iostream>
-int main 
+#include <string>
+int main
 {
+<<<<<<< HEAD
 	//программа выводит, сообщение приветсвия 
 	//conflict check
 	string name;
 	cin>>name;
 	cout<<"Hello world from "<<name; 
 }
+=======
+  //программа выводит сообщение приветсвия
+  string name;
+  cin >> name;
+  cout << "Hello world from " << name;
+}
+>>>>>>> HelloWorld new style
